44b8587815fd1dd3a086d604c4a77185cd1d2c1e
Dey <ddey4@uic.edu>
2022-11-07 17:50:24 -0600

    Drishika and Supriya MeetSync loading Page

--- meetsync-app/src/App.js
+++ meetsync-app/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MeetSync Loading...
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ckclassrooms/final-project-proposal-meetsync"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          MeetSync github
         </a>
       </header>
     </div>
